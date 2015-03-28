89a067dddcf276456e481a94587196493b8c6d70
Rafal Galka <rafal.galka@modernweb.pl>
2015-03-28 21:42:17 +0100

    code format - no functional changes

--- src/Pimcore/Composer/Installer.php
+++ src/Pimcore/Composer/Installer.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 
 namespace Pimcore\Composer;
 
@@ -11,11 +11,12 @@ use Composer\Util\Filesystem;
 
 class Installer implements InstallerInterface
 {
-	private $composer; 
+    private $composer;
     private $downloadManager;
     private $filesystem;
 
-	 public function __construct(IOInterface $io, Composer $composer, $type = 'library', Filesystem $filesystem = null)
+    public function __construct(IOInterface $io, Composer $composer, $type = 'library',
+                                Filesystem $filesystem = null)
     {
         $this->composer = $composer;
         $this->downloadManager = $composer->getDownloadManager();
@@ -27,56 +28,57 @@ class Installer implements InstallerInterface
      */
     public function getInstallPath(PackageInterface $package)
     {
-	
-		$docRootName = "./www"; 
-		if($configDocRoot = $this->composer->getConfig()->get("document-root-path")) {
-			$docRootName = rtrim($configDocRoot,"/");
-		}
-	
+        $docRootName = './www';
+        if ($configDocRoot = $this->composer->getConfig()->get('document-root-path')) {
+            $docRootName = rtrim($configDocRoot, '/');
+        }
+
         return $docRootName . '/';
     }
 
-	/**
+    /**
      * {@inheritDoc}
      */
-	public function install(InstalledRepositoryInterface $repo, PackageInterface $package) {
-		$installPath = $this->getInstallPath($package);
-		
-		if (!is_dir($installPath)) {
+    public function install(InstalledRepositoryInterface $repo, PackageInterface $package)
+    {
+        $installPath = $this->getInstallPath($package);
+
+        if (!is_dir($installPath)) {
             mkdir($installPath, 0777, true);
         }
         $this->downloadManager->download($package, $installPath);
-		
-		// cleanup
-		@unlink($installPath . "build.xml");
-		@unlink($installPath . "composer.json");
-		@unlink($installPath . "phpdox.xml.dist");
-		@unlink($installPath . "phpunit.xml.dist");
-		@unlink($installPath . "phpunit-no-coverage.xml.dist");
-		
-		rename($installPath . "plugins_example", $installPath . "plugins");
-		rename($installPath . "website_example", $installPath . "website");
-		
-		$this->recursiveDelete($installPath . "update");
-		$this->recursiveDelete($installPath . "build");
-		$this->recursiveDelete($installPath . "tests");
-		$this->recursiveDelete($installPath . ".svn");
-	}
-	
-	/**
+
+        // cleanup
+        @unlink($installPath . 'build.xml');
+        @unlink($installPath . 'composer.json');
+        @unlink($installPath . 'phpdox.xml.dist');
+        @unlink($installPath . 'phpunit.xml.dist');
+        @unlink($installPath . 'phpunit-no-coverage.xml.dist');
+
+        rename($installPath . 'plugins_example', $installPath . 'plugins');
+        rename($installPath . 'website_example', $installPath . 'website');
+
+        $this->recursiveDelete($installPath . 'update');
+        $this->recursiveDelete($installPath . 'build');
+        $this->recursiveDelete($installPath . 'tests');
+        $this->recursiveDelete($installPath . '.svn');
+    }
+
+    /**
      * {@inheritDoc}
      */
     public function isInstalled(InstalledRepositoryInterface $repo, PackageInterface $package)
     {
         return false;
     }
-	
-	/**
+
+    /**
      * {@inheritDoc}
      */
-    public function update(InstalledRepositoryInterface $repo, PackageInterface $initial, PackageInterface $target)
+    public function update(InstalledRepositoryInterface $repo, PackageInterface $initial,
+                           PackageInterface $target)
     {
-        throw new \InvalidArgumentException("not supported");
+        throw new \InvalidArgumentException('not supported');
     }
 
     /**
@@ -84,9 +86,9 @@ class Installer implements InstallerInterface
      */
     public function uninstall(InstalledRepositoryInterface $repo, PackageInterface $package)
     {
-        throw new \InvalidArgumentException("not supported");
+        throw new \InvalidArgumentException('not supported');
     }
-	
+
     /**
      * {@inheritDoc}
      */
@@ -94,44 +96,44 @@ class Installer implements InstallerInterface
     {
         return 'pimcore-core' === $packageType;
     }
-	
-	
-	protected function recursiveDelete ($directory, $empty = true) { 
 
-		if(substr($directory,-1) == "/") { 
-			$directory = substr($directory,0,-1); 
-		} 
 
-		if(!file_exists($directory) || !is_dir($directory)) { 
-			return false; 
-		} elseif(!is_readable($directory)) { 
-			return false; 
-		} else { 
-			$directoryHandle = opendir($directory);
-			$contents = ".";
+    protected function recursiveDelete($directory, $empty = true)
+    {
+        if (substr($directory, -1) == '/') {
+            $directory = substr($directory, 0, -1);
+        }
 
-			while ($contents) {
-				$contents = readdir($directoryHandle);
-				if(strlen($contents) && $contents != '.' && $contents != '..') {
-					$path = $directory . "/" . $contents; 
-					
-					if(is_dir($path)) { 
-						$this->recursiveDelete($path); 
-					} else { 
-						unlink($path); 
-					} 
-				} 
-			} 
-			
-			closedir($directoryHandle); 
+        if (!file_exists($directory) || !is_dir($directory)) {
+            return false;
+        } elseif (!is_readable($directory)) {
+            return false;
+        } else {
+            $directoryHandle = opendir($directory);
+            $contents = '.';
 
-			if($empty == true) { 
-				if(!rmdir($directory)) {
-					return false; 
-				} 
-			} 
-			
-			return true; 
-		} 
-	}
+            while ($contents) {
+                $contents = readdir($directoryHandle);
+                if (strlen($contents) && $contents != '.' && $contents != '..') {
+                    $path = $directory . '/' . $contents;
+
+                    if (is_dir($path)) {
+                        $this->recursiveDelete($path);
+                    } else {
+                        unlink($path);
+                    }
+                }
+            }
+
+            closedir($directoryHandle);
+
+            if ($empty == true) {
+                if (!rmdir($directory)) {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
 }
